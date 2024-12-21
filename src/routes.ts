8e8a9269906133103f120866825722a32d221e5e
okborges <rafael.okborges@gmail.com>
2024-12-20 22:20:30 -0300

    cria rota de produtos

--- src/routes.ts
+++ src/routes.ts
@@ -5,19 +5,28 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
+
+import { CreateProductController } from './controllers/product/CreateProductController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
+// ROTAS DE USUÁRIO
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 
+// ROTAS DE CATEGORIA
 router.post(
   '/category',
   isAuthenticated,
   new CreateCategoryController().handle
 );
+router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+// ROTAS DE PRODUTO
+router.post('/product', isAuthenticated, new CreateProductController().handle);
 
 export { router };
