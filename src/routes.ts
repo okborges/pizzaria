48fbd3f9fff91a524b4f5b294fae904bc225d30b
okborges <rafael.okborges@gmail.com>
2025-01-13 08:44:16 -0300

    rota login

--- src/routes.ts
+++ src/routes.ts
@@ -30,7 +30,7 @@ const uploud = multer(uploudConfig.upload('./tmp'));
 
 // ROTAS DE USUÁRIO
 router.post('/users', new CreateUserController().handle);
-router.post('/session', new AuthUserController().handle);
+router.post('/login', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 // ROTAS DE CATEGORIA
